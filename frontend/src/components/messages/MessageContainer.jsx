ebfb1c1d4f3cabd832b72bd065dff4456c30c8fc
KRISHNAKUMARPS2002 <krishnakumarps@gmail.com>
2024-02-15 18:56:11 +0530

    Full project commit

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,17 +3,21 @@ import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
   const { selectedConversation, setSelectedConversation } = useConversation();
 
   useEffect(() => {
+    // cleanup function (unmounts)
     return () => setSelectedConversation(null);
   }, [setSelectedConversation]);
 
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {selectedConversation ? (
+      {!selectedConversation ? (
+        <NoChatSelected />
+      ) : (
         <>
           {/* Header */}
           <div className="bg-slate-500 px-4 py-2 mb-2">
@@ -22,12 +26,9 @@ const MessageContainer = () => {
               {selectedConversation.fullName}
             </span>
           </div>
-
           <Messages />
           <MessageInput />
         </>
-      ) : (
-        <NoChatSelected />
       )}
     </div>
   );
@@ -35,10 +36,11 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 Krishna ❄</p>
+        <p>Welcome 👋 {authUser.fullName} ❄</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
